000212e03a7d20676509b18cdbd096b6ff4e7872
KevinSola <lol40553@gmail.com>
2024-02-12 14:50:59 -0500

    creacion clase EmpresaTransporte
    se crfeo la clase EmpresaTransporte con sus respectivos metodos

---
+++ empresaTransporte/src/main/java/co/edu/uniquindio/programacion2/EmpresaTransporte.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.programacion2;
+
+public class EmpresaTransporte {
+    private String nombre;
+
+    public EmpresaTransporte (String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre (){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+}
