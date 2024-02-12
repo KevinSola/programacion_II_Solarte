6b18a08586f20837df06ad969bc2fb02b0f88098
KevinSola <lol40553@gmail.com>
2024-02-12 17:47:22 -0500

    creacion VehivulosPasajeros
    se creo la clase VehiculoPasajeros

---
+++ empresaTransporte/src/main/java/co/edu/uniquindio/programacion2/VehiculoPasajero.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.programacion2;
+
+public class VehiculoPasajero extends Vehiculo{
+    private int numPasajeros;
+    public VehiculoPasajero(String placa, String modelo, int marca, int color, int numPasajeros){
+        super(placa, modelo, marca, color);
+        this.numPasajeros = numPasajeros;
+    }
+
+    public int getNumPasajeros(){
+        return numPasajeros;
+    }
+
+    public void setNumPasajeros(int numPasajeros) {
+        this.numPasajeros = numPasajeros;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", cantidadPasajeros=" + numPasajeros;
+    }
+}
